e988d0af3d108faba9e69ce644cb62ed914755e1
mykolanazaruk1992 <mykolanazaruk1992@gmail.com>
2017-08-31 14:14:34 +0300

    Create_Random_Line

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,7 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp2
-{
+{class Matrix
+    {
+        static readonly object locker = new object();
+        Random rand;
+        const string lit = "QWERTYUIOPASDFGHJKLZXCVBNM123456789";
+        public int Colunm { get; set; }
+        public Matrix(int col)
+        {
+            Colunm = col;
+            rand = new Random((int)DateTime.Now.Ticks);
+        }
+        private char GetChar()
+        {
+            return lit.ToCharArray()[rand.Next(0, 35)];
+        }
+        public void Move()
+        {
+
+        }
     class Program
     {
         static void Main(string[] args)
